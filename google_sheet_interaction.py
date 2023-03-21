118ec48bbaa2051b4779d20216b958675432a5e6
Victor Abdukarimov <viredmi@gmail.com>
2023-03-21 11:38:46 +0300

    Added valute convert func

--- google_sheet_interaction.py
+++ google_sheet_interaction.py
@@ -1,9 +1,31 @@
 import os
 
+import requests
+from xml.etree import ElementTree
+
 import httplib2
 from googleapiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
+
+from pprint import pprint
+
+
+
+
+def get_usd_exchange_rate():
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    response.raise_for_status()
+    tree = ElementTree.fromstring(response.content)
+    for elem in tree.iter('Valute'):
+        code = elem.find('CharCode').text
+        if code == 'USD':
+            usd_exchange = elem.find('Value').text
+            return float(usd_exchange.replace(',', '.'))
+
+
+def upload_to_db():
+    pass
 
 
 def main():
@@ -23,7 +45,12 @@ def main():
     service = discovery.build('sheets', 'v4', http=authorization)
     request = service.spreadsheets().values().batchGet(
         spreadsheetId=spreadsheet_id, ranges=['Лист1'])
-    print(request.execute())
+    rows = request.execute()['valueRanges'][0]['values']
+    for row in rows[1:]:
+        usd_price = int(row[2])
+        usd_exchange_rate = get_usd_exchange_rate()
+        rub_price = int(usd_price * usd_exchange_rate)
+        print(rub_price)
 
 
 if __name__ == '__main__':
